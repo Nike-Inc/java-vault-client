61d2fc5d4d412bdefcfc744a58ef5e99462213cd
Shaun Ford <shaun.ford@nike.com>
2017-04-28 17:25:43 -0700

    Add HTTP header that includes client version

---
+++ src/test/java/com/nike/vault/client/ClientVersionTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017 Nike, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nike.vault.client;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests ClientVersion class
+ */
+public class ClientVersionTest {
+
+    @Test
+    public void test_that_version_file_exists() {
+        getClass().getClassLoader().getResourceAsStream(ClientVersion.CLIENT_VERSION_PROPERTY_FILE);
+    }
+
+    @Test
+    public void test_that_version_is_not_null() {
+        assertNotNull(ClientVersion.getVersion());
+    }
+
+    @Test
+    public void test_that_header_value_includes_right_prefix() {
+
+        String result = ClientVersion.getClientHeaderValue();
+        assertTrue(StringUtils.contains(result, ClientVersion.HEADER_VALUE_PREFIX));
+    }
+}
