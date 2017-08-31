2a708fdaec1657ef31f27af8892054d58775f449
Todd Lisonbee <tlisonbee@users.noreply.github.com>
2017-08-31 08:39:27 -0700

    Exposing more HTTP settings for the VaultAdminClient (#12)

--- src/main/java/com/nike/vault/client/VaultClientFactory.java
+++ src/main/java/com/nike/vault/client/VaultClientFactory.java
@@ -31,15 +31,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class VaultClientFactory {
 
-    private static final int DEFAULT_TIMEOUT = 15;
-    private static final TimeUnit DEFAULT_TIMEOUT_UNIT = TimeUnit.SECONDS;
+    private static final int DEFAULT_TIMEOUT = 15_000;
+    private static final TimeUnit DEFAULT_TIMEOUT_UNIT = TimeUnit.MILLISECONDS;
 
     /**
      * A VaultAdminClient may need to make many requests to Vault simultaneously.
-     *
-     * (Default value in OkHttpClient was 5).
+     * <p>
+     * (Default value in OkHttpClient for maxRequests was 64 and maxRequestsPerHost was 5).
      */
-    private static final int DEFAULT_MAX_REQUESTS_PER_HOST = 200;
+    private static final int DEFAULT_MAX_REQUESTS = 200;
+    private static final Map<String, String> DEFAULT_HEADERS = new HashMap<>();
 
     /**
      * Basic factory method that will build a Vault client that
@@ -70,7 +71,7 @@ public class VaultClientFactory {
     /**
      * Factory method that allows for a user defined Vault URL resolver and credentials provider.
      *
-     * @param vaultUrlResolver    URL resolver for Vault
+     * @param vaultUrlResolver         URL resolver for Vault
      * @param vaultCredentialsProvider Credential provider for acquiring a token for interacting with Vault
      * @return Vault client
      */
@@ -83,9 +84,9 @@ public class VaultClientFactory {
      * Factory method that allows a user to define default HTTP defaultHeaders to be added to every HTTP request made from the
      * VaultClient. The user can also define their Vault URL resolver and credentials provider.
      *
-     * @param vaultUrlResolver          URL resolver for Vault
-     * @param vaultCredentialsProvider  Credential provider for acquiring a token for interacting with Vault
-     * @param defaultHeaders            Map of default header names and values to add to every HTTP request
+     * @param vaultUrlResolver         URL resolver for Vault
+     * @param vaultCredentialsProvider Credential provider for acquiring a token for interacting with Vault
+     * @param defaultHeaders           Map of default header names and values to add to every HTTP request
      * @return Vault client
      */
     public static VaultClient getClient(final UrlResolver vaultUrlResolver,
@@ -103,11 +104,11 @@ public class VaultClientFactory {
         return new VaultClient(vaultUrlResolver,
                 vaultCredentialsProvider,
                 new OkHttpClient.Builder()
-                    .connectTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                    .writeTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                    .readTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                    .build(),
-               headers.build());
+                        .connectTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
+                        .writeTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
+                        .readTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
+                        .build(),
+                headers.build());
     }
 
     /**
@@ -122,13 +123,15 @@ public class VaultClientFactory {
      * @return Vault admin client
      */
     public static VaultAdminClient getAdminClient() {
-        return new VaultAdminClient(new DefaultVaultUrlResolver(),
+        return getAdminClient(new DefaultVaultUrlResolver(),
                 new DefaultVaultCredentialsProviderChain(),
-                new OkHttpClient.Builder()
-                        .connectTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                        .writeTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                        .readTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                        .build());
+                DEFAULT_MAX_REQUESTS,
+                DEFAULT_MAX_REQUESTS,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_HEADERS
+        );
     }
 
     /**
@@ -139,48 +142,78 @@ public class VaultClientFactory {
      * @return Vault admin client
      */
     public static VaultAdminClient getAdminClient(final UrlResolver vaultUrlResolver) {
-        return getAdminClient(vaultUrlResolver, new DefaultVaultCredentialsProviderChain(), DEFAULT_MAX_REQUESTS_PER_HOST);
+        return getAdminClient(vaultUrlResolver,
+                new DefaultVaultCredentialsProviderChain(),
+                DEFAULT_MAX_REQUESTS,
+                DEFAULT_MAX_REQUESTS,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_HEADERS
+        );
     }
 
     /**
      * Factory method that allows for a user defined Vault URL resolver and credentials provider.
      *
-     * @param vaultUrlResolver    URL resolver for Vault
+     * @param vaultUrlResolver         URL resolver for Vault
      * @param vaultCredentialsProvider Credential provider for acquiring a token for interacting with Vault
      * @return Vault admin client
      */
     public static VaultAdminClient getAdminClient(final UrlResolver vaultUrlResolver,
                                                   final VaultCredentialsProvider vaultCredentialsProvider) {
-        return getAdminClient(vaultUrlResolver, vaultCredentialsProvider, DEFAULT_MAX_REQUESTS_PER_HOST);
+        return getAdminClient(vaultUrlResolver,
+                vaultCredentialsProvider,
+                DEFAULT_MAX_REQUESTS,
+                DEFAULT_MAX_REQUESTS,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_HEADERS
+        );
     }
 
     /**
      * Factory method that allows for a user defined Vault URL resolver and credentials provider.
      *
-     * @param vaultUrlResolver    URL resolver for Vault
+     * @param vaultUrlResolver         URL resolver for Vault
      * @param vaultCredentialsProvider Credential provider for acquiring a token for interacting with Vault
-     * @param maxRequestsPerHost Max Requests per Host used by the dispatcher
+     * @param maxRequestsPerHost       Max Requests per Host used by the dispatcher
      * @return Vault admin client
      */
     public static VaultAdminClient getAdminClient(final UrlResolver vaultUrlResolver,
                                                   final VaultCredentialsProvider vaultCredentialsProvider,
                                                   final int maxRequestsPerHost) {
-        return getAdminClient(vaultUrlResolver, vaultCredentialsProvider, maxRequestsPerHost, new HashMap<String, String>());
+        return getAdminClient(vaultUrlResolver,
+                vaultCredentialsProvider,
+                DEFAULT_MAX_REQUESTS,
+                maxRequestsPerHost,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_HEADERS);
     }
 
     /**
      * Factory method that allows a user to define default HTTP headers to be added to every HTTP request made from the
      * VaultClient. The user can also define their Vault URL resolver and credentials provider.
      *
-     * @param vaultUrlResolver          URL resolver for Vault
-     * @param vaultCredentialsProvider  Credential provider for acquiring a token for interacting with Vault
-     * @param defaultHeaders            Map of default header names and values to add to every HTTP request
+     * @param vaultUrlResolver         URL resolver for Vault
+     * @param vaultCredentialsProvider Credential provider for acquiring a token for interacting with Vault
+     * @param defaultHeaders           Map of default header names and values to add to every HTTP request
      * @return Vault client
      */
     public static VaultAdminClient getAdminClient(final UrlResolver vaultUrlResolver,
                                                   final VaultCredentialsProvider vaultCredentialsProvider,
                                                   final Map<String, String> defaultHeaders) {
-        return getAdminClient(vaultUrlResolver, vaultCredentialsProvider, DEFAULT_MAX_REQUESTS_PER_HOST, defaultHeaders);
+        return getAdminClient(vaultUrlResolver,
+                vaultCredentialsProvider,
+                DEFAULT_MAX_REQUESTS,
+                DEFAULT_MAX_REQUESTS,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                defaultHeaders);
     }
 
     /**
@@ -196,11 +229,43 @@ public class VaultClientFactory {
                                                   final VaultCredentialsProvider vaultCredentialsProvider,
                                                   final int maxRequestsPerHost,
                                                   final Map<String, String> defaultHeaders) {
+        return getAdminClient(vaultUrlResolver,
+                vaultCredentialsProvider,
+                DEFAULT_MAX_REQUESTS,
+                maxRequestsPerHost,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                DEFAULT_TIMEOUT,
+                defaultHeaders);
+    }
+
+    /**
+     * Factory method that allows the user to completely configure the VaultClient.
+     *
+     * @param vaultUrlResolver         URL resolver for Vault
+     * @param vaultCredentialsProvider Credential provider for acquiring a token for interacting with Vault
+     * @param maxRequests              Max HTTP Requests allowed in-flight
+     * @param maxRequestsPerHost       Max HTTP Requests per Host
+     * @param connectTimeoutMillis     HTTP connect timeout in milliseconds
+     * @param readTimeoutMillis        HTTP read timeout in milliseconds
+     * @param writeTimeoutMillis       HTTP write timeout in milliseconds
+     * @param defaultHeaders           Map of default header names and values to add to every HTTP request
+     * @return Vault admin client
+     */
+    public static VaultAdminClient getAdminClient(final UrlResolver vaultUrlResolver,
+                                                  final VaultCredentialsProvider vaultCredentialsProvider,
+                                                  final int maxRequests,
+                                                  final int maxRequestsPerHost,
+                                                  final int connectTimeoutMillis,
+                                                  final int readTimeoutMillis,
+                                                  final int writeTimeoutMillis,
+                                                  final Map<String, String> defaultHeaders) {
         if (defaultHeaders == null) {
             throw new IllegalArgumentException("Default headers cannot be null.");
         }
 
         Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequests(maxRequests);
         dispatcher.setMaxRequestsPerHost(maxRequestsPerHost);
 
         Headers.Builder headers = new Headers.Builder();
@@ -211,11 +276,11 @@ public class VaultClientFactory {
         return new VaultAdminClient(vaultUrlResolver,
                 vaultCredentialsProvider,
                 new OkHttpClient.Builder()
-                    .connectTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                    .writeTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                    .readTimeout(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT_UNIT)
-                    .dispatcher(dispatcher)
-                    .build(),
+                        .connectTimeout(connectTimeoutMillis, DEFAULT_TIMEOUT_UNIT)
+                        .writeTimeout(writeTimeoutMillis, DEFAULT_TIMEOUT_UNIT)
+                        .readTimeout(readTimeoutMillis, DEFAULT_TIMEOUT_UNIT)
+                        .dispatcher(dispatcher)
+                        .build(),
                 headers.build());
     }
 }
